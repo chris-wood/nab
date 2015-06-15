2b9964912aa438cd1532185ddfe7955e514c1316
Christopher Wood <christopherwood07@gmail.com>
2015-06-15 09:16:55 -0700

    go nab

---
+++ src/nab.py
@@ -0,0 +1,12 @@
+from BeautifulSoup import BeautifulSoup
+import urllib2
+
+class Resource(object):
+    def __init__(self):
+        pass
+
+class Podcast(Resource):
+    pass
+
+class BlogPost(Resource):
+    pass
